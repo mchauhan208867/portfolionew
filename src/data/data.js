55f175371efa0731cfdc400ef24defc18afbfa6d
mchauhan208867 <mchauhan208867@gmail.com>
2024-08-04 14:34:37 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/jqf4YGHR/istockphoto-183819034-612x612.jpg',
+    title: "Dice Game",
+    desc: "Developed a dice game using JavaScript , React JS , Html , Css. ",
+    link: "https://dice-game-plum-mu.vercel.app/",
+    git: "https://github.com/mchauhan208867/Dice_game",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/Y0XWzzcq/images.jpg',
+    title: "Firebase Contact app",
+    link: "https://firebase-contact-app-rre8.vercel.app/",
+    git: "https://github.com/mchauhan208867/firebase-contact-app",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/PJhqvpPf/download.png',
+    title: "Text Utils",
+    desc: " Developed an Web application for text processing tasks",
+    link: "https://mayank-delta.vercel.app/",
+    git: "https://github.com/mchauhan208867/textutils",
+  }
+  
+];
